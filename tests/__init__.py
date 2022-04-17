0285180868a0693940fdd20b7823a64c420aeb22
Kevo <me@kevo.link>
2022-04-17 12:55:08 +0200

    Move to `forum-database-schemas`

--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from flarum_migrations import fm_create_engine
-from flarum_migrations.models.flarum import ALL_FLARUM_MODELS
-
-
-ENGINE = fm_create_engine(ALL_FLARUM_MODELS, 'sqlite:///tests/test.db', echo=True)
